271a1892f1daf400c3d178876b90512b90bece2c
Mario Loredo <al43722@un.edu.mx>
2025-06-30 16:55:52 -0600

    diseño 1

--- obj/Debug/net8.0/PortalPsicologia.AssemblyInfo.cs
+++ obj/Debug/net8.0/PortalPsicologia.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortalPsicologia")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+587cf2666dbceb3e82967ea3f4188ea256288f46")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+378ddd48827a4b0fde79b763a546792048ebbdc3")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortalPsicologia")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortalPsicologia")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
