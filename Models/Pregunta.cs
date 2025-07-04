322067fbb3d7c8f30a1724161f7450cdf3e20a62
Mario Loredo <al43722@un.edu.mx>
2025-07-03 20:21:00 -0600

    avance diseño y errores

--- Models/Pregunta.cs
+++ Models/Pregunta.cs
@@ -7,6 +7,7 @@ namespace PortalPsicologia.Models
         public int PreguntaId { get; set; }
 
         [Required(ErrorMessage = "El texto de la pregunta es obligatorio.")]
+        [StringLength(400, ErrorMessage = "La pregunta no debe superar los 300 caracteres.")]
         public string Texto { get; set; }
 
         public bool Activa { get; set; } = true;
--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -6,18 +6,22 @@ namespace PortalPsicologia.Models
     public class Usuario
     {
         public int UsuarioId { get; set; }
-        [Required]
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+            [StringLength(100, ErrorMessage = "El nombre no puede tener más de 100 caracteres")]
         public string Nombre { get; set; }
 
-        [Required]
+         [Required(ErrorMessage = "El correo es obligatorio")]
+    [EmailAddress(ErrorMessage = "Correo electrónico inválido")]
+    [StringLength(100, ErrorMessage = "El correo no puede tener más de 100 caracteres")]
         [Remote(action: "VerificarCorreoUnico", controller: "Usuario", AdditionalFields = "UsuarioId", ErrorMessage = "Este correo ya está registrado")]
         public string Correo { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "La matrícula es obligatoria")]
+    [RegularExpression(@"^[0-9]{1,5}$", ErrorMessage = "La matrícula debe contener solo números (máximo 5)")]
         [Remote(action: "VerificarMatriculaUnica", controller: "Usuario", AdditionalFields = "UsuarioId", ErrorMessage = "Esta matrícula ya está registrada")]
         public string Matricula { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "El rol es obligatorio")]
         public string Rol { get; set; }
     }
 }
